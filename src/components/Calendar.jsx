f924328e25f81758a96f2d27b96679485f0532af
Ahmad Kamran <ahmad.eritheia@gmail.com>
2022-06-05 03:44:37 +0500

    added register worker to register the events

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -28,6 +28,103 @@ class Calendar extends React.Component {
     );
   }
 
+  renderDays() {
+    const dateFormat = "dddd";
+    const days = [];
+
+    let startDate = dateFns.startOfWeek(this.state.currentMonth);
+
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+
+    return <div className="days row">{days}</div>;
+  }
+
+  renderCells = () => {
+    const { currentMonth, selectedDate, displayBookedCells } = this.state;
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
+
+    const dateFormat = "D";
+    const rows = [];
+
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = dateFns.format(day, dateFormat);
+        let random = Math.floor(Math.random() * (31)) + 1;
+        days.push(
+          <div
+            className={`col cell ${
+              displayBookedCells && dateFns.isSameMonth(day, monthStart) && (parseInt(formattedDate) <= random)
+                ? 'booked' : ''
+              } ${
+              !dateFns.isSameMonth(day, monthStart)
+                ? "disabled"
+                : dateFns.isSameDay(day, selectedDate) ? "selected" : ""
+            }`}
+            key={day}
+          >
+            <span className="number">{formattedDate}</span>
+            <span className="bg">{formattedDate}</span>
+          </div>
+        );
+        day = dateFns.addDays(day, 1);
+      }
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+    return <div className="body">{rows}</div>;
+  }
+
+  nextMonth = () => {
+    this.setState({
+      currentMonth: dateFns.addMonths(this.state.currentMonth, 1),
+      displayBookedCells: false
+    });
+  };
+
+  prevMonth = () => {
+    this.setState({
+      currentMonth: dateFns.subMonths(this.state.currentMonth, 1),
+      displayBookedCells: false
+    });
+  };
+
+  generateRandomBookings = () => {
+    this.setState({
+      displayBookedCells: true
+    });
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="calendar">
+          {this.renderHeader()}
+          {this.renderDays()}
+          {this.renderCells()}
+        </div>
+        <button className="btn mt-10" onClick={this.generateRandomBookings}>
+          Generate Random Bookings
+        </button>
+      </div>
+    );
+  }
 }
 
 export default Calendar;
